8bf472eb2be5ad73e1dfc99f1d8231b6756eb6d8
Aryadeep123 <94110846+Aryadeep123@users.noreply.github.com>
2023-04-03 10:26:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My First Console Project");
+
+var first_name="Aryadeep";
+var last_name="Bakshi";
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a+b;
+console.log(add);
+
+var sub = a-b;
+console.log(sub);
+
+var mult = a*b;
+console.log(mult);
+
+var div = a/b;
+console.log(div);
